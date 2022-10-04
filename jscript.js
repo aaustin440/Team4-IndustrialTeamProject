4a8fcc5db29dadab75faaaac3ec90a5d7d04aefe
robbieG123 <55398030+robbieG123@users.noreply.github.com>
2022-10-04 14:23:43 +0100

    javascript for increasing font size

---
+++ jscript.js
@@ -0,0 +1,12 @@
+    //var ruleInfo = document.getElementById("rule-info");
+    //var exceptionInfo = document.getElementById("exception-info");
+    //var exceptionDate = document.getElementById("exception-date");
+    var cont = document.getElementById("container");
+    function changeSizeByBtn(size) {
+
+        // Set value of the parameter as fontSize
+        cont.style.fontSize = size;
+       // ruleInfo.style.fontSize = size;
+        //exceptionInfo.style.fontSize = size;
+        //exceptionDate.style.fontSize = size;
+    }
